66452d0e0d8b783bc0793a442ae624e3892cb933
Ashwani Kr <86672936+raaichu121@users.noreply.github.com>
2024-06-27 21:07:15 +0545

    Add files via upload

---
+++ blogsite/blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from blog.models import category
+
+# Create your views here.
+
+def home(request):
+    categories = category.objects.all()
+    context={'categories': categories}
+    return render(request,'index.html',context)
